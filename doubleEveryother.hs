2acda400debfa921794fb1579b792e7e5c421e86
Katarina Persson <133044734+ykpersson@users.noreply.github.com>
2025-06-22 18:23:13 +0200

    Update doubleEveryother.hs

--- doubleEveryother.hs
+++ doubleEveryother.hs
@@ -1,7 +1,7 @@
 doubleEveryOther :: [Integer] -> [Integer]
-doubleEveryOther xs = go xs 0
+doubleEveryOther xs = go xs (length xs -1)
   where
     go [] _ = []
     go (x:xs) i
-      | even i    = (2 * x) : go xs (i + 1)
-      | otherwise = x       : go xs (i + 1)
+      | even i  = (2 * x): go xs (i - 1)
+      | otherwise = x: go xs (i -1)
